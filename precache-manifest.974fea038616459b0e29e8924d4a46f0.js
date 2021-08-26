86c18485009b56194290fea60416c03d37fa8d29
Tengrix <damir861@gmail.com>
2021-08-25 23:11:40 -0400

    Updates

---
+++ precache-manifest.974fea038616459b0e29e8924d4a46f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428f6d9992313ae9308448304a1b772e",
+    "url": "/workproject/index.html"
+  },
+  {
+    "revision": "31b9c665bca62e573bf9",
+    "url": "/workproject/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/workproject/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/workproject/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b9c665bca62e573bf9",
+    "url": "/workproject/static/js/main.5638a612.chunk.js"
+  },
+  {
+    "revision": "ef0996c959f705ce406b",
+    "url": "/workproject/static/js/runtime-main.fc39f830.js"
+  }
+]);
